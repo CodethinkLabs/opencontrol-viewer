e000ad8ed9e6621a692ac532c28b5f0428c5f852
Jim MacArthur <jim.macarthur@codethink.co.uk>
2016-10-19 17:24:38 +0100

    Add a CSS stylesheet

---
+++ stylesheet.py
@@ -0,0 +1,16 @@
+style = """
+<style>
+li li {
+    list-style-type: square;
+    font-size: smaller;
+    border-style: none;
+}
+
+li {
+    list-style-type: square;
+    font-size: xx-large;
+    border-style: solid;
+}
+
+</style>
+"""
